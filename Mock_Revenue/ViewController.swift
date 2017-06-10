5c66618aad4ccdca86df64024b9f3ab7d4d3ea71
Van Nhan <nguyenvannhan0810@gmail.com>
2017-06-10 14:23:25 +0700

    Initial Commit

---
+++ Mock_Revenue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mock_Revenue
+//
+//  Created by Cntt11 on 6/10/17.
+//  Copyright © 2017 Cntt11. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
